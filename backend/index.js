ffe97c706d8f32494b000aefb4ad8493742052d8
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2024-01-20 01:17:12 +1300

    feat: add setup, endpoints and basic error handling (adapted from exercises)
    this is the reason this commit is so large, I amessentially copy pasting from the code I completed in the exercises unashamedly because I have not memorised the syntax

---
+++ backend/index.js
@@ -0,0 +1,62 @@
+require('dotenv').config()
+const express = require('express')
+const Person = require('./models/person')
+
+const app = express()
+
+app.use(express.static('dist'))
+app.use(express.json())
+
+app.get('/api/people', (request, response) => {
+  Person.find({})
+    .then(people => response.json(people))
+})
+
+app.get('/api/people/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/people', (request, response, next) => {
+  const body = request.body
+
+  const newPerson = new Person({
+    name: body.name,
+    birthdate: body.birthdate,
+  })
+
+  newPerson.save()
+    .then(savedPerson => response.json(savedPerson))
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  console.log(request.body)
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).json({ error: 'malformatted id' })
+  } else {
+    return response.status(400).json({ error: error.message })
+  }
+  // TODO: add error handling for ValidationError
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
